b6cfe77975fa214beebd028511c4f7d7de450798
madhusudan9332 <madhusudangautam003@gmail.com>
2024-07-10 20:47:07 +0530

    added all code

---
+++ index.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config();
+
+const DBModel = mongoose.model('resturents',mongoose.Schema({
+        name:{
+            type:String,
+        },
+        location: {
+            type:String,
+        },
+        rating: {
+            type:Number,
+        },
+        menu:{
+            type:Array
+        },
+        "open time": {
+            type:String
+        },
+        distance: {
+            type:String
+        }
+      }
+))
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("DB connected successfully"))
+  .catch((err) => console.log("DB connection failed"));
+
+app.get("/restaurants", async (req, res) => {
+  const resturents = await DBModel.find();
+  res.json(resturents);
+});
+
+app.get("/restaurants/:id", async (req, res) => {
+  const resturent = await DBModel.findById(req.params.id);
+  res.json(resturent);
+});
+
+app.get("/restaurants/search/:query", (req, res) => {
+    let query = req.params.query || "";
+    query = query.toLowerCase();
+    console.log(query);
+    const results = restaurants.filter(
+      (restaurant) =>
+        restaurant.name.toLowerCase().includes(query) ||
+        restaurant.cuisine.toLowerCase().includes(query)
+    );
+    res.json(results);
+  });
+  
+  // Get restaurants by category of food
+  app.get("/restaurants/food/:category", (req, res) => {
+    const category = req.params.category.toLowerCase();
+    const results = restaurants.filter((restaurant) =>
+      restaurant.foods.some((food) => food.toLowerCase().startsWith(category))
+    );
+    res.json(results);
+  });
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+})
